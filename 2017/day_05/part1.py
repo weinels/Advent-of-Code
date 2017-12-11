3ddb3292c2dc866b283e0f9cdd10f4edbeb00ed9
weinels <weinels@gmail.com>
2017-12-10 20:06:33 -0700

    Completed part one.

---
+++ 2017/day_05/part1.py
@@ -0,0 +1,18 @@
+inst = []
+
+with open('part1.input') as f:
+	for line in f:
+		inst += [int(line)]		
+step = 0
+i = 0
+try:
+	while True:
+		#print("Before i:{0}; inst[i]:{1}; step:{2}; inst:{3}".format(i,inst[i], step, inst))
+		next = inst[i]
+		inst[i] += 1
+		i += next
+		step +=1
+		#print("After  i:{0}; inst[i]:{1}; step:{2}; inst:{3}".format(i,inst[i], step, inst))
+
+except LookupError:
+	print("Steps = {0}".format(step))
